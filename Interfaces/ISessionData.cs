828cab05e85e016925a21d34774d42505dd09824
Diego Bonfil <bonfdie@gmail.com>
2024-11-26 16:35:34 -0500

    Regresa la lista de Claims y los muestra en un campo

--- Interfaces/ISessionData.cs
+++
@@ -1,8 +0,0 @@
-﻿using Blazor_Server_App_Login.Login;
-namespace Blazor_Server_App_Login.Interfaces
-{
-    public interface ISessionData
-    {
-        public SessionDTO sessionState { get; }
-    }
-}
---
+++ Models/Values.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor_Server_App_Login.Models
+{
+    public class Values
+    {
+        public string Value { get; set; }
+
+        public string Text { get; set; }
+    }
+}
